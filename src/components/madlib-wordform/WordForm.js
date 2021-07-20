822707172ac00dd5aba6f7e70cb577fcd15377c9
stesun2 <ss27@illinois.edu>
2021-07-20 17:02:48 -0500

    managed to create a text input of word objects

--- src/components/madlib-wordform/WordForm.js
+++ src/components/madlib-wordform/WordForm.js
@@ -5,9 +5,21 @@ class WordForm extends Component {
   onInputChange(key, index, event) {
     this.props.onInputChange(key, event.currentTarget.value, index)
   }
+  createAllInputs() {
+    let output = []
+    for (let i = 0; i < this.props.words.length; i++) {
+      let word = this.props.words[i]
+      output.push(
+        <input type="text" id={word.key} placeholder={word.label} />
+      )
+    }
+    return output
+  }
   render() {
     return (
-      <div></div>
+      <div>
+        { this.createAllInputs() }
+      </div>
     )
   }
 }
